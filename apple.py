c5bc73d8ce35ff5e8cab83c530adec9cabd7ced4
P210 <53483555+P210@users.noreply.github.com>
2021-05-06 18:29:01 +0530

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,9 @@
+print("Do you know cut apple?")
+print("Steps-:"
+"1.	To cut the fruits we need a knife and a fruit example (Apple)."
+"2.	To cut the apple put the knife on the top of the apple and start cutting it."
+"3.	Remove all the red color peels of it."
+"4.	Now cut down the peach part."
+"5.	From the middle cut the apple into pieces. "
+"6.	Now remove the seed of all the pieces."
+"7.	Now it’s ready to eat.")
